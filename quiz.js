ef156b1bee7728208423674dec18fab45ff221b5
Albin <albin.nojback@gmail.com>
2020-12-17 15:19:39 +0100

    Add Name in result

--- quiz.js
+++ quiz.js
@@ -9,6 +9,15 @@ function enterQuiz()
 
     let answers = document.querySelectorAll("input[type='radio']:checked");
 
+    let name = document.querySelector("input[type='text']").value;
+
+    if (name === "")
+    {
+    } else
+    {
+        name = name + ", "
+    }
+
     for (let index = 0; index < answers.length; index++) {
         if (parseInt(answers[index].value) > 0)
         {
@@ -21,24 +30,24 @@ function enterQuiz()
 
     if (questionsAnswered === 0)
     {
-        answerText = "Du svarade inte på nått!"
+        answerText = name + "Du svarade inte på nått!";
     } else if (answerTotal <= (max / 3)) {
-        answerText = "Du verkar pretty sad, gå o hämta din favorit filt o chilla lite!"
+        answerText = name + "Du verkar pretty sad, gå o hämta din favorit filt o chilla lite!";
     } else if (answerTotal >= (2 * (max / 3))) {
-        answerText = "Du har det fan pretty good, tuffa på grabben"
+        answerText = name + "Du har det fan pretty good, tuffa på grabben";
     } else {
-        answerText = "Du är ganska normal, men alla behöver lite kärlek <3!"
+        answerText = name + "Du är ganska normal, men alla behöver lite kärlek <3!";
     }
 
     document.querySelector(".result-text").innerHTML = answerText;
 
-    document.querySelector(".form").classList.toggle("invisible");
+    document.querySelector(".quiz").classList.toggle("invisible");
     document.querySelector(".result").classList.toggle("invisible");
 }
 
 function reset()
 {
-    document.querySelector(".form").classList.toggle("invisible");
+    document.querySelector(".quiz").classList.toggle("invisible");
     document.querySelector(".result").classList.toggle("invisible");
 
     document.querySelector(".form").reset();
